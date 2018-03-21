cd2444e20dd8ec1bb05a0e2ef53064f079009e92
Natan <natanbc@users.noreply.github.com>
2018-03-21 16:01:14 -0300

    this isn't nullable

--- src/main/java/com/github/natanbc/reliqua/util/ResponseMapper.java
+++ src/main/java/com/github/natanbc/reliqua/util/ResponseMapper.java
@@ -2,11 +2,12 @@ package com.github.natanbc.reliqua.util;
 
 import okhttp3.Response;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.IOException;
 
 @FunctionalInterface
 public interface ResponseMapper<T> {
     @Nullable
-    T apply(@Nullable Response body) throws IOException;
+    T apply(@Nonnull Response body) throws IOException;
 }
